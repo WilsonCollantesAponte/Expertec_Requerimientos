45dbdbe3425556f0cc61d5f567103fb7d790d0f7
Gerson Sanchez <gsanchezdiaz@outlook.com>
2023-05-04 15:16:59 -0500

    primeros pasos con react router DOM

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./assets/css/base/base.css";
+import "./assets/css/componentes/card.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/sobre" element={<Sobre />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
