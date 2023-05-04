44c42d73c66093d04251697a483e241214a49044
Gerson Sanchez <gsanchezdiaz@outlook.com>
2023-05-04 15:52:16 -0500

    Agregue componente Page404 que indica que no existe la pagina

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./assets/css/componentes/card.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Sobre from "./pages/Sobre";
+import Page404 from "./pages/Page404";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/sobre" element={<Sobre />} />
+                <Route path="*" element={<Page404 />} />
             </Routes>
         </Router>
     );
