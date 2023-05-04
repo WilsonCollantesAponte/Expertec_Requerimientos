98df0607cc771c3f864fdf85000ceb7e92b76fe1
Gerson Sanchez <gsanchezdiaz@outlook.com>
2023-05-04 16:31:46 -0500

    agregue componente Header que se muestra en todas las pages

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Sobre from "./pages/Sobre";
 import Page404 from "./pages/Page404";
+import Header from "./components/Header";
 
 function App() {
     return (
         <Router>
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/sobre" element={<Sobre />} />
