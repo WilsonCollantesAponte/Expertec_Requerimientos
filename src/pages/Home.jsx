ae531b455c95e17159a751b78845f8ff72ed35ec
Melvin <mrodriguez@x-pertec.com>
2023-05-18 09:59:33 -0500

    creamos el componente listacategorias para filtrar las tarjetas por categorias y luego lo mostramos en el home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import ListPost from "../components/ListPost";
+import ListCategories from "../components/ListCategories";
 const Home = () => {
     return (
         <main>
             <div className="container">
                 <h2 className="title-page">Pet noticias</h2>
             </div>
+            <ListCategories />
             <ListPost url={"/posts"} />
         </main>
     );
